6fb3384d871c3857d460c3d258f0b82c485118ee
anasjameel300 <anasjameel300@gmail.com>
2025-11-15 15:20:20 +0530

    Add margin-bottom to hero component

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -59,7 +59,7 @@ export default function Hero() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.3 }}
-              className="flex flex-col items-center gap-6"
+              className="flex flex-col items-center gap-6 mb-12"
             >
               {/* Decorative SVG */}
               <svg
